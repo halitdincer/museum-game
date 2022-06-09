54e3350d0157c51f8e571a72fa22167f193eab15
halitdincer <github@halitdincer.com>
2022-06-09 14:58:26 -0400

    added state machine

--- state.py
+++ state.py
@@ -1,7 +1,21 @@
-class State :
+
+class StateMachine:
+    def __init__(self):
+        self.state = State()
+
+    def on_event(self, event):
+        self.state = self.state.on_event(event)
+
+class State:
 
     def __init__(self):
         pass
 
-    def on_event(self, event):
+    def on_event(self, event): 
         pass
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return self.__class__.__name__
